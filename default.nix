800aba3d367e5860409962278bda4f1979789e6c
Kayh <basedlainpilled@gmail.com>
2023-12-30 17:21:13 -0500

    add --target web to wasm-bindgen

--- default.nix
+++ default.nix
@@ -28,7 +28,7 @@ in {
     '';
     installPhase = ''
       mkdir -p $out/pages/wasm
-      wasm-bindgen target/wasm32-unknown-unknown/wasm-release/vrm_viewer.wasm --out-dir $out/pages/wasm --no-typescript
+      wasm-bindgen --out-dir $out/pages/wasm --target web --no-typescript target/wasm32-unknown-unknown/wasm-release/vrm_viewer.wasm 
       wasm-opt -Oz $out/pages/wasm/vrm_viewer_bg.wasm -o $out/pages/wasm/vrm_viewer_bg.wasm
     '';
   });
