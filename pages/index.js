dbb5a9420af8d64fa776c739af61efbd7bf01a33
Kayh <basedlainpilled@gmail.com>
2023-12-30 19:05:53 -0500

    js drag and drop preventDefault

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,15 @@
 import init from "./wasm/vrm_viewer.js";
 
+// Prevent default drag and drop behavior
+document.body.addEventListener("dragover", (e) => {
+  e.preventDefault();
+});
+
+document.body.addEventListener("drop", (e) => {
+  e.preventDefault();
+});
+
+// Start Bevy
 console.log("Initializing WASM");
 
 init().then(() => {
